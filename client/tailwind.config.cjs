3ac26b34957d4ddc545dc0f9b826d55bc5c901ca
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-13 23:14:05 +0630

    before client

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
