ad0428c6bd2f44c5338aa52355af62813b95230a
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-13 16:15:42 +0630

    test api

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://shinedsun:fOdxYl7Rh2HlcYus@firstmernapp.1o6psip.mongodb.net/firstmernapp?retryWrites=true&w=majority")
+app.use("/auth", userRouter);
+
+mongoose.connect(
+  "mongodb+srv://shinedsun:fOdxYl7Rh2HlcYus@firstmernapp.1o6psip.mongodb.net/firstmernapp?retryWrites=true&w=majority"
+);
 
 app.listen(3000, () => console.log("server start"));
+
+import { userRouter } from "./routes/users.js";
