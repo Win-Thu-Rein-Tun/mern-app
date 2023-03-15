0f594b0b0a8f4e5282aa978e4f35dc99766ef578
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-15 18:34:07 +0630

    before client

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipesModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -18,6 +19,30 @@ router.post("/", async (req, res) => {
   try {
     const response = await recipe.save();
     res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipesModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.saveRecipes.push(recipe);
+    await user.save();
+    res.json({ saveRecipes: user.saveRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/saveRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    const saveRecipes = await RecipesModel.find({
+      _id: { $id: user.saveRecipes },
+    });
+    res.json({saveRecipes})
   } catch (err) {
     res.json(err);
   }
