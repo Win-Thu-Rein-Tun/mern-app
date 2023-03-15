44fd9882798910e4bb5173784977bb3e0f527356
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-15 15:13:10 +0630

    api okay

--- server/src/models/Recipes.js
+++ server/src/models/Recipes.js
@@ -4,6 +4,7 @@ const RecipesSchema = new mongoose.Schema({
   username: { type: String, required: true },
   ingredients: [{ type: String, required: true }],
   instructions: {type: String, required: true},
+  imgUrl: {type: String, required: true},
   cookingTime: {type: Number, required: true},
   userOwner: {
     type: mongoose.Schema.Types.ObjectId,
