1d99361d9e3488307f07753cdbe084bbb485a654
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-17 13:56:30 +0630

    fetch okay

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState("");
+
+  useEffect(() => {
+    const fetchRecipes = async (e) => {
+      try {
+        const response = await axios.get("http://localhost:3000/recipes");
+        // setRecipes(response.data);
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return <div>Home</div>;
 };
 
