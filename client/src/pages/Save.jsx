239b5fe9e69a72573e7abb16738835aa728d9a0e
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-18 13:22:29 +0630

    everything is okay

--- client/src/pages/Save.jsx
+++ client/src/pages/Save.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { useGetUserID } from "../hooks/useGetUserID";
 
 const Save = () => {
-  const [recipes, setRecipes] = useState([]);
   const [savedRecipes, setSavedRecipes] = useState([]);
 
   const userID = useGetUserID();
@@ -24,64 +23,42 @@ const Save = () => {
     fetchSaveRecipes();
   }, []);
 
-  const saveRecipes = async (recipeID) => {
-    try {
-      const response = await axios.put("http://localhost:3000/recipes", {
-        recipeID,
-        userID,
-      });
-      setSavedRecipes(response.data.saveRecipes)
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const isSavedRecipes = (id) => savedRecipes.includes(id);
-
   return (
     <div className="bg-secondary h-screen flex justify-center">
-      <div className="sm:px-16 px-6 bg-primary text-white overflow-auto scrollbar-hide">
-        <ul className="pb-16 ">
-          {recipes.map((recipe) => (
-            <li
-              key={recipe._id}
-              className="flex flex-col gap-4 bg-secondary py-6 px-6 rounded-lg my-10"
-            >
-              <div className="flex justify-between items-center">
-                <h1 className="font-bold text-[24px] text-first">
-                  {recipe.name}
-                </h1>
-                <button
-                  type="button"
-                  onClick={() => saveRecipes(recipe._id)}
-                  disabled={isSavedRecipes(recipe._id)}
-                  className={`bg-second hover:bg-teal-600 text-white font-bold py-2 px-4 rounded justify-end flex ${
-                    isSavedRecipes(recipe._id)
-                      ? "disabled:opacity-50 cursor-not-allowed"
-                      : ""
-                  }`}
-                >
-                  {isSavedRecipes(recipe._id) ? "Saved" : "Save"}
-                </button>
-              </div>
-              <h2 className="font-semibold"> Ingredients :</h2>
-              <ul className="px-8 list-disc">
-                {recipe.ingredients.map((ingredient, index) => (
-                  <li key={index}>{ingredient}</li>
-                ))}
-              </ul>
+      {!savedRecipes ? (
+        <div></div>
+      ) : (
+        <div className="sm:px-16 px-6 bg-primary text-white overflow-auto scrollbar-hide">
+          <ul className="pb-16 ">
+            {savedRecipes.map((recipe) => (
+              <li
+                key={recipe._id}
+                className="flex flex-col gap-4 bg-secondary py-6 px-6 rounded-lg my-10"
+              >
+                <div className="flex justify-between items-center">
+                  <h1 className="font-bold text-[24px] text-first">
+                    {recipe.name}
+                  </h1>
+                </div>
+                <h2 className="font-semibold"> Ingredients :</h2>
+                <ul className="px-8 list-disc">
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                  ))}
+                </ul>
 
-              <p>{recipe.instructions}</p>
-              <img
-                src={recipe.imgUrl}
-                alt="recipe.name"
-                className="object-fit rounded-xl"
-              />
-              <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <p>{recipe.instructions}</p>
+                <img
+                  src={recipe.imgUrl}
+                  alt="recipe.name"
+                  className="object-fit rounded-xl"
+                />
+                <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
