f795ce6167c3681d9a3a125289d93b31d8ef07b2
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-18 11:29:35 +0630

    still saved button color

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -57,6 +57,7 @@ const Home = () => {
               key={recipe._id}
               className="flex flex-col gap-4 bg-secondary py-6 px-6 rounded-lg my-10"
             >
+              {isSavedRecipes(recipe._id) && <h1>already saved</h1>}
               <div className="flex justify-between items-center">
                 <h1 className="font-bold text-[24px] text-first">
                   {recipe.name}
@@ -65,9 +66,9 @@ const Home = () => {
                   type="button"
                   onClick={() => saveRecipes(recipe._id)}
                   disabled={isSavedRecipes(recipe._id)}
-                  className="bg-second hover:bg-teal-600 text-white font-bold py-2 px-4 rounded justify-end flex"
+                  className="bg-second hover:bg-teal-600 text-white font-bold py-2 px-4 rounded justify-end flex disabled"
                 >
-                  Save
+                  {isSavedRecipes(recipe._id) ? "Saved" : "Save"}
                 </button>
               </div>
               <h2 className="font-semibold"> Ingredients :</h2>
