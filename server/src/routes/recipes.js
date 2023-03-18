dd7ac6f310e78843802d9c61f89e5dc2c3ae16ef
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-18 15:32:12 +0630

    authorization

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import mongoose from "mongoose";
 import { RecipesModel } from "../models/Recipes.js";
 import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./users.js";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   const recipe = new RecipesModel(req.body);
   try {
     const response = await recipe.save();
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/", async (req, res) => {
+router.put("/", verifyToken, async (req, res) => {
   try {
     const recipe = await RecipesModel.findById(req.body.recipeID);
     const user = await UserModel.findById(req.body.userID);
@@ -36,14 +36,14 @@ router.put("/", async (req, res) => {
   }
 });
 
-router.get("/saveRecipes/ids/:userID", async (req, res) => {
+router.get("/saveRecipes/ids/:userID", verifyToken, async (req, res) => {
   try {
     const user = await UserModel.findById(req.params.userID);
     res.json({ saveRecipes: user?.saveRecipes });
   } catch (err) {
     res.json(err);
   }
-})
+});
 
 router.get("/saveRecipes/:userID", async (req, res) => {
   try {
