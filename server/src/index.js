04729b08bf66b47dfd024841bf141be6f84f8149
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-19 19:34:02 +0630

    before i .env

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
 
+
 const app = express();
 
 app.use(express.json());
