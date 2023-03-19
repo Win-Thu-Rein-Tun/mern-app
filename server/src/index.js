714468daf99ea9f858a44348226829f7f4639363
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-19 20:06:07 +0630

    okay .env

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,13 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
+const DATABASE = process.env.DATABASE;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
@@ -13,8 +17,6 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
-mongoose.connect(
-  "mongodb+srv://shinedsun:fOdxYl7Rh2HlcYus@firstmernapp.1o6psip.mongodb.net/firstmernapp?retryWrites=true&w=majority"
-);
+mongoose.connect(DATABASE);
 
-app.listen(3001, () => console.log("server start"));
+app.listen(PORT, () => console.log("server start"));
